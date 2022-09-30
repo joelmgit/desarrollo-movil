eaee4d488b7de6bd8c18f0102b293719bf89bb8a
Joel Mora <joelm.firebase@gmail.com>
2022-09-30 10:21:21 -0600

    Trabajo en clase: segunda semana

--- app/src/main/java/com/lugares/viewmodel/LugarViewModel.kt
+++ app/src/main/java/com/lugares/viewmodel/LugarViewModel.kt
@@ -1,13 +1,27 @@
 package com.lugares.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.lugares.data.LugarDatabase
+import com.lugares.model.Lugar
+import com.lugares.repository.LugarRepository
+import kotlinx.coroutines.launch
 
-class LugarViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+class LugarViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: LugarRepository
+    private val getLugares: LiveData<List<Lugar>>
+    init {
+        val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
+        repository = LugarRepository(lugarDao)
+        getLugares = repository.getLugares
     }
-    val text: LiveData<String> = _text
+    suspend fun addLugar(lugar: Lugar) {
+        viewModelScope.launch { repository.addLugar(lugar) }
+    }
+    suspend fun updateLugar(lugar: Lugar) {
+        viewModelScope.launch { repository.updateLugar(lugar) }
+    }
+    suspend fun deleteLugar(lugar: Lugar) {
+        viewModelScope.launch { repository.deleteLugar(lugar) }
+    }
 }
